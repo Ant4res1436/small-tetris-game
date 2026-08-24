81873f53cef9dd2faccbc372235fa097c3cbcc4d
Ant4res1436 <205226378+Ant4res1436@users.noreply.github.com>
2026-08-24 13:58:06 +0200

    Untrack build.bash, ignore build artifacts

--- build.bash
+++
@@ -1,2 +0,0 @@
-gcc -std=c99 -g main.c src/*.c ~/cold-clear/target/release/libcold_clear.a -I/home/antares/cold-clear/c-api -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -Wall -mbmi2 -o tetris
-# ./tetris
